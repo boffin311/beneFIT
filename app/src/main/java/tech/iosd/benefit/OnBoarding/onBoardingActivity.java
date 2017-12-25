d18ef78015e21355d2a0936e655137d0b40db96f
anubhavmalik <anubhavmalikdeveloper@gmail.com>
2017-12-26 00:26:36 +0530

    Added onBoarding Skeleton

---
+++ app/src/main/java/tech/iosd/benefit/OnBoarding/onBoardingActivity.java
@@ -0,0 +1,15 @@
+package tech.iosd.benefit.OnBoarding;
+
+import android.os.Bundle;
+
+import agency.tango.materialintroscreen.MaterialIntroActivity;
+
+public class onBoardingActivity extends MaterialIntroActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        /*TODO: Add custom slides here for onBoarding Fragment*/
+    }
+}
