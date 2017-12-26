ca92e97041f813df2687e5d77f6b05aa5fbd0bde
anubhavmalik <anubhavmalikdeveloper@gmail.com>
2017-12-26 07:19:04 +0530

    Google Sign in added

---
+++ app/src/main/java/tech/iosd/benefit/Constants/AuthConstants.java
@@ -0,0 +1,9 @@
+package tech.iosd.benefit.Constants;
+
+/**
+ * Created by Anubhav on 26-12-2017.
+ */
+
+public class AuthConstants {
+    public final static String googleClientId= "712691277447-dvcnq5ghjvqvg028j93sm64o2pq5lk1g.apps.googleusercontent.com";
+}
--- app/src/main/java/tech/iosd/benefit/OnBoarding/loginFragment.java
+++ app/src/main/java/tech/iosd/benefit/OnBoarding/loginFragment.java
@@ -1,32 +1,147 @@
 package tech.iosd.benefit.OnBoarding;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 import agency.tango.materialintroscreen.SlideFragment;
+import tech.iosd.benefit.Constants.AuthConstants;
+import tech.iosd.benefit.R;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * Created by Anubhav on 26-12-2017.
  */
 
 public class loginFragment extends SlideFragment {
+    private static final int RC_SIGN_IN = 1;
+    SignInButton mGoogleSignInButton;
+    GoogleApiClient mGoogleApiClient;
+    private FirebaseAuth mAuth;
+    FirebaseAuth.AuthStateListener mAuthListener;
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        View v = inflater.inflate(R.layout.onboarding_login, container, false);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener=new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                //TODO: update this method when required
+            }
+        };
+        mGoogleSignInButton = (SignInButton) v.findViewById(R.id.google_sign_in_button);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(AuthConstants.googleClientId)
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(getContext().getApplicationContext())
+                .enableAutoManage(getActivity(), new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+                    }
+                })
+        .addApi(Auth.GOOGLE_SIGN_IN_API,gso)
+        .build();
+
+
+        mGoogleSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
+
+
+        return v;
+    }
+
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                            //TODO: DO SOMETHING when user succesfully signed in
+//                            Log.d(TAG, "signInWithCredential:success");
+                        }
+
+                    }
+                });
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+//            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                if(result.isSuccess()){
+                GoogleSignInAccount account = result.getSignInAccount();
+                firebaseAuthWithGoogle(account);
+                }
+            } catch (Exception e) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e);
+                Toast.makeText(getContext(), "Sign In Failed", Toast.LENGTH_SHORT).show();
+                // ...
+            }
+
+         }
     }
 
     @Override
     public int backgroundColor() {
-        return super.backgroundColor();
+        return R.color.colorAccent;
     }
 
     @Override
     public int buttonsColor() {
-        return super.buttonsColor();
+        return R.color.colorPrimaryDark;
     }
 
     @Override
