aaa45ed13b76c7df4f51a90918bc01412c758459
anubhavmalik <anubhavmalikdeveloper@gmail.com>
2017-12-27 07:29:28 +0530

    phone auth ka baaki kam kal uthne k baad.. Gn

--- app/src/main/java/tech/iosd/benefit/Authentication/OtpActivity.java
+++ app/src/main/java/tech/iosd/benefit/Authentication/OtpActivity.java
@@ -1,5 +1,6 @@
 package tech.iosd.benefit.Authentication;
 
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,5 +12,11 @@ public class OtpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_otp);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.otp_auth_area,new GetOtpFragment())
+                .commit();
     }
 }
