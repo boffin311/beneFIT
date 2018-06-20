51bd7f64ec5c9bed11fbbf5ca600330dd6f1a3f7
Prerak Mann <mannprerak2@gmail.com>
2018-06-21 00:10:57 +0530

    Added google fit dummy code to get daily counts of steps, calories, distance or counts between interval

--- app/src/main/java/tech/iosd/benefit/AccountFragment.java
+++ app/src/main/java/tech/iosd/benefit/AccountFragment.java
@@ -1,6 +1,7 @@
 package tech.iosd.benefit;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
@@ -9,6 +10,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,12 +24,26 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptionsExtension;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.Scopes;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Scope;
 import com.google.android.gms.fitness.Fitness;
 import com.google.android.gms.fitness.FitnessOptions;
+import com.google.android.gms.fitness.data.Bucket;
+import com.google.android.gms.fitness.data.DataPoint;
+import com.google.android.gms.fitness.data.DataSet;
 import com.google.android.gms.fitness.data.DataType;
+import com.google.android.gms.fitness.data.Field;
+import com.google.android.gms.fitness.request.DataReadRequest;
+import com.google.android.gms.fitness.result.DataReadResponse;
+import com.google.android.gms.fitness.result.DataReadResult;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.squareup.picasso.Picasso;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 public class AccountFragment extends Fragment implements
     GoogleApiClient.ConnectionCallbacks,
@@ -37,7 +53,6 @@ public class AccountFragment extends Fragment implements
     FragmentManager fm;
 
     TextView googleFit;
-
     public AccountFragment() {
         // Required empty public constructor
     }
@@ -48,16 +63,22 @@ public class AccountFragment extends Fragment implements
         View rootView = inflater.inflate(R.layout.fragment_account, container, false);
         ctx = rootView.getContext();
         fm = getFragmentManager();
-
         googleFit = rootView.findViewById(R.id.googleFit);
+
+
+        FitnessOptions fitnessOptions = FitnessOptions.builder()
+                .addDataType(DataType.TYPE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
+                .addDataType(DataType.AGGREGATE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
+                .addDataType(DataType.TYPE_DISTANCE_DELTA,FitnessOptions.ACCESS_READ)
+                .build();
+
+        if (GoogleSignIn.hasPermissions(GoogleSignIn.getLastSignedInAccount(getActivity()), fitnessOptions)) {
+            //removing plus icon
+                    removeGoogleFitIcon();
+        }
         googleFit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                FitnessOptions fitnessOptions = FitnessOptions.builder()
-                        .addDataType(DataType.TYPE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
-                        .addDataType(DataType.AGGREGATE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
-                        .build();
-
                 if (!GoogleSignIn.hasPermissions(GoogleSignIn.getLastSignedInAccount(getActivity()), fitnessOptions)) {
                     GoogleSignIn.requestPermissions(
                             getActivity(), // your activity
@@ -75,18 +96,105 @@ public class AccountFragment extends Fragment implements
         return rootView;
     }
 
+    private void removeGoogleFitIcon(){
+        googleFit.setCompoundDrawables(null,null,null,null);
+    }
+
     private void accessGoogleFit() {
-        GoogleSignInOptionsExtension fitnessOptions =
-                FitnessOptions.builder()
+        GoogleSignInOptionsExtension fitnessOptions = FitnessOptions.builder()
                         .addDataType(DataType.TYPE_ACTIVITY_SAMPLES, FitnessOptions.ACCESS_READ)
                         .build();
-
         GoogleSignInAccount gsa = GoogleSignIn.getAccountForExtension(getActivity(), fitnessOptions);
-
         Task<Void> response = Fitness.getRecordingClient(getActivity(), gsa)
                 .subscribe(DataType.TYPE_ACTIVITY_SAMPLES);
-        // play services starts recording stuff now
+        // play services starts recording stuff now ( ^ ^ )
+
+
+        Fitness.getHistoryClient(getContext(),GoogleSignIn.getLastSignedInAccount(getContext()))
+                .readDailyTotal(DataType.TYPE_STEP_COUNT_DELTA)
+                .addOnSuccessListener(new OnSuccessListener<DataSet>() {
+                    @Override
+                    public void onSuccess(DataSet dataSet) {
+                        DateFormat dateFormat = DateFormat.getDateInstance();
+                        DateFormat timeFormat = DateFormat.getTimeInstance();
+                        String x = "";
+                        for(DataPoint dp : dataSet.getDataPoints()){
+                            x += "Steps Today: "+ dp.getValue(Field.FIELD_STEPS) + "\n";
+                        }
+
+                        Toast.makeText(getContext(),x,Toast.LENGTH_SHORT).show();
+                    }
+                });
+        Fitness.getHistoryClient(getContext(),GoogleSignIn.getLastSignedInAccount(getContext()))
+                .readDailyTotal(DataType.TYPE_CALORIES_EXPENDED)
+                .addOnSuccessListener(new OnSuccessListener<DataSet>() {
+                    @Override
+                    public void onSuccess(DataSet dataSet) {
+                        DateFormat dateFormat = DateFormat.getDateInstance();
+                        DateFormat timeFormat = DateFormat.getTimeInstance();
+                        String x = "";
+                        for(DataPoint dp : dataSet.getDataPoints()){
+                            x += "Calories Burnt: "+ dp.getValue(Field.FIELD_CALORIES) + "\n";
+                        }
+                        Toast.makeText(getContext(),x,Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+        Fitness.getHistoryClient(getContext(),GoogleSignIn.getLastSignedInAccount(getContext()))
+                .readDailyTotal(DataType.TYPE_DISTANCE_DELTA)
+                .addOnSuccessListener(new OnSuccessListener<DataSet>() {
+                    @Override
+                    public void onSuccess(DataSet dataSet) {
+                        DateFormat dateFormat = DateFormat.getDateInstance();
+                        DateFormat timeFormat = DateFormat.getTimeInstance();
+                        String x = "";
+                        for(DataPoint dp : dataSet.getDataPoints()){
+                            x += "Distance Today: "+ dp.getValue(Field.FIELD_DISTANCE) + "\n";
+                        }
+                        Toast.makeText(getContext(),x,Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
+
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(new Date());
+        long endTime = cal.getTimeInMillis();
+        cal.add(Calendar.WEEK_OF_YEAR, -1);
+        long startTime = cal.getTimeInMillis();
+        Log.w("t",String.valueOf(startTime));
+
+        DataReadRequest readRequest = new DataReadRequest.Builder()
+                .aggregate(DataType.TYPE_STEP_COUNT_DELTA, DataType.AGGREGATE_STEP_COUNT_DELTA)
+                .setTimeRange(startTime, endTime, TimeUnit.MILLISECONDS)
+                .bucketByTime(7,TimeUnit.DAYS)
+                .build();
+
+        Fitness.getHistoryClient(getContext(),GoogleSignIn.getLastSignedInAccount(getContext()))
+                .readData(readRequest)
+                .addOnSuccessListener(new OnSuccessListener<DataReadResponse>() {
+                    @Override
+                    public void onSuccess(DataReadResponse dataReadResponse) {
+                        DateFormat dateFormat = DateFormat.getDateInstance();
+                        DateFormat timeFormat = DateFormat.getTimeInstance();
+                        String x = "";
+
+                        for(Bucket bucket: dataReadResponse.getBuckets()){
+                            x += "Bucket \n";
+                            for(DataSet dataSet: bucket.getDataSets()){
+                                x+="  Dataset \n";
+                                for(DataPoint dp: dataSet.getDataPoints()){
+                                    x += "\nStart: "+dateFormat.format(dp.getStartTime(TimeUnit.MILLISECONDS)) + " " + timeFormat.format(dp.getStartTime(TimeUnit.MILLISECONDS));
+                                    x += "\nEnd: "+dateFormat.format(dp.getEndTime(TimeUnit.MILLISECONDS)) + " " + timeFormat.format(dp.getEndTime(TimeUnit.MILLISECONDS));
+                                    x += "\n Steps: "+ dp.getValue(Field.FIELD_STEPS);
+                                }
+                            }
+                        }
+                        Toast.makeText(getContext(),x,Toast.LENGTH_LONG).show();
+                    }
+                });
     }
+
 
     @Override
     public void onConnected(@Nullable Bundle bundle) {
@@ -108,6 +216,7 @@ public class AccountFragment extends Fragment implements
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (resultCode == Activity.RESULT_OK) {
             if (requestCode == GOOGLE_FIT_PERMISSIONS_REQUEST_CODE) {
+                removeGoogleFitIcon();
                 accessGoogleFit();
             }
         }
