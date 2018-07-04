181bcd78ce1040159b405b34ca70bdbb50b6df0b
Prerak Mann <mannprerak2@gmail.com>
2018-07-04 23:29:30 +0530

    Integrated Database to store chat

--- app/src/main/java/tech/iosd/benefit/Chat/ChatDatabase.java
+++ app/src/main/java/tech/iosd/benefit/Chat/ChatDatabase.java
@@ -8,8 +8,11 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
+import tech.iosd.benefit.Author;
+import tech.iosd.benefit.DashboardFragments.Chat;
 import tech.iosd.benefit.Message;
 
 public class ChatDatabase extends SQLiteOpenHelper {
@@ -49,16 +52,43 @@ public class ChatDatabase extends SQLiteOpenHelper {
         db.insert(TABLE_NAME, null, contentValues);
     }
 
-    public List<Message> getMessages(){
+    public List<Message> getMessages(){//doesn't set author , use if else in chat activity and reuse author objects
         List<Message> allMessages = new ArrayList<>();
         SQLiteDatabase db = this.getReadableDatabase();
         Cursor res = db.rawQuery("select * from "+TABLE_NAME,null);
+
+        Author nutritionist = new Author("49", "Nutritionist", "https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/LetterN.svg/1200px-LetterN.svg.png");
+        Author coach = new Author("50", "Coach", "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Letter_c.svg/1200px-Letter_c.svg.png");
+        Author me = new Author("51", "Me");
+
         res.moveToFirst();
         while (!res.isAfterLast()){
+            Author author;
+            if(res.getInt(res.getColumnIndex(COULMN_AUTHOR))==Chat.ID_ME){
+                author = me;
+            }else if(res.getInt(res.getColumnIndex(COULMN_AUTHOR))==Chat.ID_COACH){
+                author = coach;
+            }else {//nutritionist
+                author = nutritionist;
+            }
+                Message message = new Message(
+                    String.valueOf(res.getInt(res.getColumnIndex(COULMN_AUTHOR))),
+                    res.getString(res.getColumnIndex(COULMN_MESSAGE)),
+                    author,
+                    new Date(res.getLong(res.getColumnIndex(COULMN_TIME)))
+                    );
+            allMessages.add(message);
 
+            res.moveToNext();
         }
 
         res.close();
         return allMessages;
     }
+
+    public void deleteAllMessages(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.execSQL("delete from "+TABLE_NAME);
+    }
+
 }
--- app/src/main/java/tech/iosd/benefit/DashboardFragments/Chat.java
+++ app/src/main/java/tech/iosd/benefit/DashboardFragments/Chat.java
@@ -3,6 +3,7 @@ package tech.iosd.benefit.DashboardFragments;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -44,6 +45,7 @@ import io.socket.emitter.Emitter;
 import tech.iosd.benefit.Adapters.MessageAdapter;
 import tech.iosd.benefit.Author;
 import tech.iosd.benefit.Chat.ChatApplication;
+import tech.iosd.benefit.Chat.ChatDatabase;
 import tech.iosd.benefit.Message;
 import tech.iosd.benefit.Model.DatabaseHandler;
 import tech.iosd.benefit.R;
@@ -54,6 +56,8 @@ public class Chat extends Fragment implements MessageInput.InputListener
     FragmentManager fm;
     ProgressDialog progressDialog;
 
+    View rootView;
+
     String TAG = "tag";
 
     private DatabaseHandler db;
@@ -63,9 +67,9 @@ public class Chat extends Fragment implements MessageInput.InputListener
 
     private static final int TYPING_TIMER_LENGTH = 600;
 
-    private static final int ID_ME = 0;
-    private static final int ID_COACH = 1;
-    private static final int ID_NUTRITIONIST = 2;
+    public static final int ID_ME = 0;
+    public static final int ID_COACH = 1;
+    public static final int ID_NUTRITIONIST = 2;
 
 
     private MessagesList mMessagesView;
@@ -81,46 +85,13 @@ public class Chat extends Fragment implements MessageInput.InputListener
     private MessagesListAdapter<Message> adapter;
     private MessageInput messageInput;
 
+    //if changing author details also change in chatDatabase getMessages()
     Author nutritionist = new Author("49", "Nutritionist", "https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/LetterN.svg/1200px-LetterN.svg.png");
     Author coach = new Author("50", "Coach", "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Letter_c.svg/1200px-Letter_c.svg.png");
     Author me = new Author("51", "Me");
 
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        progressDialog = new ProgressDialog(getContext());
-        progressDialog.setMessage("Connecting..");
-        progressDialog.setCancelable(false);
-//        progressDialog.show();
+    ChatDatabase chatDatabase;
 
-        db = new DatabaseHandler(getContext());
-
-        ChatApplication app = (ChatApplication) getActivity().getApplication();
-        mSocket = app.getSocket();
-
-        attemptLogin();
-
-        mSocket.on(Socket.EVENT_CONNECT,onConnect);
-        mSocket.on(Socket.EVENT_DISCONNECT,onDisconnect);
-        mSocket.on(Socket.EVENT_CONNECT_ERROR, onConnectError);
-        mSocket.on(Socket.EVENT_CONNECT_TIMEOUT, onConnectError);
-        mSocket.on("new message", onNewMessage);
-        mSocket.on("user joined", onUserJoined);
-        mSocket.on("user left", onUserLeft);
-        mSocket.on("unauthorized",onUnAuthorised);
-        mSocket.connect();
-
-
-
-    }
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        //mAdapter = new MessageAdapter(context, mMessages);
-        if (context instanceof Activity){
-            //this.listener = (MainActivity) context;
-        }
-    }
     private void attemptLogin() {
 
         Log.w("pkmn","attemptLogin");
@@ -133,14 +104,6 @@ public class Chat extends Fragment implements MessageInput.InputListener
         mSocket.on("login", onLogin);
 
     }
-
-    private Emitter.Listener onUnAuthorised = new Emitter.Listener() {
-        @Override
-        public void call(Object... args) {
-            Toast.makeText(getContext(),"Not Authorized",Toast.LENGTH_SHORT).show();
-            progressDialog.hide();
-        }
-    };
 
     private Emitter.Listener onLogin = new Emitter.Listener() {
         @Override
@@ -159,19 +122,7 @@ public class Chat extends Fragment implements MessageInput.InputListener
         }
     };
 
-    private void addLog(String message) {
-        /*mMessages.add(new Message.Builder(Message.TYPE_LOG)
-                .message(message).build());
-        mAdapter.notifyItemInserted(mMessages.size() - 1);
-        scrollToBottom();*/
-    }
-
-    private void addParticipantsLog(int numUsers) {
-        return;
-        //addLog(getResources().getQuantityString(R.plurals.message_participants, numUsers, numUsers));
-    }
-
-    private void addMessage(String message,int authorInt) {
+    private void addMessage(String message,int authorInt) {//adds a message on receiving and on sending
 //        mMessages.add(new Message.Builder(Message.TYPE_MESSAGE)
 //                .username(username).message(message).build());
 //        mAdapter.notifyItemInserted(mMessages.size() - 1);
@@ -185,29 +136,24 @@ public class Chat extends Fragment implements MessageInput.InputListener
         else if(authorInt==ID_NUTRITIONIST) {
             adapter.addToStart(new Message(String.valueOf(ID_NUTRITIONIST), message, nutritionist, Calendar.getInstance().getTime()), true);
         }
+        //inserting to database
+        chatDatabase.insertMessage(authorInt,message,Calendar.getInstance().getTimeInMillis());
     }
 
     private void attemptSend(String msg) {
 
-        if (mUsername==null) return;
-        if(!isConnected){
+        if(mUsername==null || !isConnected){
             Toast.makeText(getContext(),"Not Connected",Toast.LENGTH_SHORT).show();
             return;
         }
-        mTyping = false;
 
         String message = msg.trim();
-        /*if (TextUtils.isEmpty(message)) {
-            mInputMessageView.requestFocus();
-            return;
-        }*/
+        if(message.equals("")) return;
 
         JSONObject msgObj = new JSONObject();
         try {
-
-            msgObj.accumulate("username", mUsername);
-            msgObj.accumulate("message", message);
-            //mInputMessageView.setText("");
+            msgObj.put("username", mUsername);
+            msgObj.put("message", message);
             addMessage(message, ID_ME);
             mSocket.emit("new message", msgObj);
 
@@ -216,16 +162,6 @@ public class Chat extends Fragment implements MessageInput.InputListener
         }
 
 
-    }
-
-    private void leave() {
-        mUsername = null;
-        mSocket.disconnect();
-        mSocket.connect();
-    }
-
-    private void scrollToBottom() {
-        //mMessagesView.scrollToPosition(mAdapter.getItemCount() - 1);
     }
 
     private Emitter.Listener onConnect = new Emitter.Listener() {
@@ -239,8 +175,7 @@ public class Chat extends Fragment implements MessageInput.InputListener
                         if (!isConnected) {
                             if (mUsername != null)
                                 mSocket.emit("add user", mUsername);
-                            Toast.makeText(getContext(),
-                                    "server connected", Toast.LENGTH_LONG).show();
+                            showStatus(true);
                             isConnected = true;
                         }
                     }
@@ -257,9 +192,8 @@ public class Chat extends Fragment implements MessageInput.InputListener
                     @Override
                     public void run() {
                         Log.i(TAG, "diconnected");
+                        showStatus(false);
                         isConnected = false;
-                        Toast.makeText(getActivity().getApplicationContext(),
-                                "disconnected", Toast.LENGTH_LONG).show();
                     }
                 });
             }
@@ -274,8 +208,7 @@ public class Chat extends Fragment implements MessageInput.InputListener
                     @Override
                     public void run() {
                         Log.e(TAG, "Error connecting");
-                        Toast.makeText(getActivity().getApplicationContext(),
-                                "error connecing", Toast.LENGTH_LONG).show();
+                        showStatus(false);
                     }
                 });
             }
@@ -301,7 +234,6 @@ public class Chat extends Fragment implements MessageInput.InputListener
 
                         } catch (JSONException e) {
                             Log.e(TAG, e.getMessage());
-                            return;
                         }
 
                     }
@@ -327,9 +259,6 @@ public class Chat extends Fragment implements MessageInput.InputListener
                             Log.e(TAG, e.getMessage());
                             return;
                         }
-
-                        addLog("joined" + username);
-                        addParticipantsLog(numUsers);
                     }
                 });
             }
@@ -353,21 +282,31 @@ public class Chat extends Fragment implements MessageInput.InputListener
                             Log.e(TAG, e.getMessage());
                             return;
                         }
-
-                        addLog("left" + username);
-                        addParticipantsLog(numUsers);
                     }
                 });
             }
         }
     };
 
+    private void showStatus(Boolean connected){
+        if(connected){
+            TextView chatLabel = rootView.findViewById(R.id.chat_lbl);
+            chatLabel.setText("Chat with your coach now");
+            ImageView statusCircle = rootView.findViewById(R.id.status_circle);
+            statusCircle.setColorFilter(Color.GREEN);
+        }else {
+            TextView chatLabel = rootView.findViewById(R.id.chat_lbl);
+            chatLabel.setText("Cannot connect to chat");
+            ImageView statusCircle = rootView.findViewById(R.id.status_circle);
+            statusCircle.setColorFilter(Color.RED);
+        }
+    }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState)
     {
-        View rootView = inflater.inflate(R.layout.dashboard_chat, container, false);
+        rootView = inflater.inflate(R.layout.dashboard_chat, container, false);
         ctx = rootView.getContext();
         fm = getFragmentManager();
 
@@ -380,16 +319,38 @@ public class Chat extends Fragment implements MessageInput.InputListener
             }
         };
 
+        progressDialog = new ProgressDialog(getContext());
+        progressDialog.setMessage("Connecting..");
+        progressDialog.setCancelable(false);
+//        progressDialog.show();
+
+        db = new DatabaseHandler(getContext());
+        chatDatabase = new ChatDatabase(getContext());
+
+        ChatApplication app = (ChatApplication) getActivity().getApplication();
+        mSocket = app.getSocket();
+
+        attemptLogin();
+
+        mSocket.on(Socket.EVENT_CONNECT,onConnect);
+        mSocket.on(Socket.EVENT_DISCONNECT,onDisconnect);
+        mSocket.on(Socket.EVENT_CONNECT_ERROR, onConnectError);
+        mSocket.on(Socket.EVENT_CONNECT_TIMEOUT, onConnectError);
+        mSocket.on("new message", onNewMessage);
+        mSocket.on("user joined", onUserJoined);
+        mSocket.on("user left", onUserLeft);
+        mSocket.connect();
 
         adapter = new MessagesListAdapter<>("0", imageLoader);
-
         //Demo Code
+        List<Message> messageList = chatDatabase.getMessages();
 
-
-        adapter.addToStart(new Message(String.valueOf(ID_COACH), "Hey, I'm your coach", coach, Calendar.getInstance().getTime()), true);
-        adapter.addToStart(new Message(String.valueOf(ID_NUTRITIONIST), "Hi, I'm your nutritionist", nutritionist, Calendar.getInstance().getTime()), true);
-        //
-
+        if(messageList.size()>0){
+            adapter.addToEnd(messageList,true);
+        }else {
+            adapter.addToStart(new Message(String.valueOf(ID_COACH), "Hey, I'm your coach", coach, Calendar.getInstance().getTime()), true);
+            adapter.addToStart(new Message(String.valueOf(ID_NUTRITIONIST), "and I'm your nutritionist \nFeel free to ask anything :) \nYour fitness is our main concern :D", nutritionist, Calendar.getInstance().getTime()), true);
+        }
 
         mMessagesView = rootView.findViewById(R.id.messagesList);
         mMessagesView.setLayoutManager(new LinearLayoutManager(getActivity()));
@@ -408,6 +369,7 @@ public class Chat extends Fragment implements MessageInput.InputListener
         });
         return rootView;
     }
+
 
     @Override
     public boolean onSubmit(CharSequence input) {
